177d7e15251424d17e1ea3cbb141a86929090aff
Arthur Ferreira <arthur.f@gec.inatel.br>
2022-05-19 14:53:35 -0300

    Add files via upload
    first commit

---
+++ Curso_SpringBoot/src/main/java/com/api/parkingControl/repositories/ParkingSpotRepository.java
@@ -0,0 +1,15 @@
+package com.api.parkingControl.repositories;
+
+import com.api.parkingControl.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+
+
+
+}
---
+++ Curso_SpringBoot/src/main/java/com/api/parkingControl/services/ParkingSpotService.java
@@ -0,0 +1,23 @@
+package com.api.parkingControl.services;
+
+import com.api.parkingControl.models.ParkingSpotModel;
+import com.api.parkingControl.repositories.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+}
